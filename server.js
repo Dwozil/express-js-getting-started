bdc11fa4dff81a5aeab362c29a98b9f7f63ac363
jgm <joaogbm@live.com>
2017-09-01 00:08:10 +0100

    added ejs view engine

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
+//Hello Route
+const app = express();
+app.get('/Hello/', function (req, res) {
+    res.send('Hello World!!');
 });
 
+//Index Route
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+//Boot Server
 app.listen(3000, function(){
     console.log('App listening on port 3000');
 });
